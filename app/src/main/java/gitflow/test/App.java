cbad8f11834b0f06d9eddb20f3ff321df7f242e9
Savannah Webb <webbsav2@isu.edu>
2022-01-28 15:21:11 -0700

    Changed App.java

--- app/src/main/java/gitflow/test/App.java
+++ app/src/main/java/gitflow/test/App.java
@@ -5,7 +5,7 @@ package gitflow.test;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World! This is my first attempt at creating an app with features!";
     }
 
     public static void main(String[] args) {
